50bad1d430e9fcebbc58563952031f6fad3905ad
Nourdine Mazali <nourdine@archistar.ai>
2023-03-14 13:16:42 +1100

    Event Hub added

--- handler.js
+++ handler.js
@@ -1,7 +1,10 @@
 'use strict';
 const DynamoDB = require("aws-sdk/clients/dynamodb");
+const EventBridge = require("aws-sdk/clients/eventbridge");
 const documentClient = new DynamoDB.DocumentClient({ region: 'us-east-1' });
 const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME;
+const eventBridge = new EventBridge({ region: 'us-east-1' });
+const EVENT_BUS_NAME = process.env.EventBusName;
 
 const send = (statusCode, data) => {
   return {
@@ -22,11 +25,51 @@ module.exports.createNote = async (event, context, cb) => {
       ConditionExpression: "attribute_not_exists(noteId)"
     }
     await documentClient.put(params).promise();
+
+    // send event
+    const eventParams = {
+      Entries: [
+        {
+          Source: 'notes-app',
+          DetailType: 'note-created',
+          Detail: JSON.stringify(data),
+          EventBusName: EVENT_BUS_NAME
+        }
+      ]
+    }
+    await eventBridge.putEvents(eventParams).promise();
+
+
     cb(null, send(201, data));
   } catch (err) {
     cb(null, send(500, err.message));
   }
 };
+
+// Path: notes/handler.js
+module.exports.processEvent = async (even) => {
+  console.log('Event: ', even);
+  let batchItemFailures = [];
+  let records = even.Records;
+  for (let record of records) {
+    let event = JSON.parse(record.body);
+    console.log('Event: ', event);
+    try {
+      const parsedBody = JSON.parse(record.body);
+
+      console.log("processing note details for " + parsedBody.detail.id);
+      console.log("processing is successful " + record.messageId);
+    } catch (err) {
+      batchItemFailures.push({
+        itemIdentifier: record.messageId,
+      });
+    }
+  }
+
+  return { batchItemFailures };
+
+}
+
 
 module.exports.updateNote = async (event, context, cb) => {
   let noteId = event.pathParameters.id;
